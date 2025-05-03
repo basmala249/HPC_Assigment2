8a052bbbf594392d8d18e70a766ada43899d4717
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2025-05-03 18:21:13 +0300

    Create Problem1_Part1.c

---
+++ Problem1_Part1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+#include <stdbool.h>
+
+bool isPrime(int n){
+  if(n <= 1)return 0;
+  if(n == 2)return 1;
+  if(n % 2 == 0) return 0;
+  for(int i = 3 ; i * i <= n ; i += 2){
+      if(n % i == 0){
+        return 0;      
+      }  
+  }
+  return 1;
+}
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    
+    int rank , numOfProcesses ;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numOfProcesses);
+    int x , r , y;
+    if(rank == 0){
+       if(numOfProcesses < 2){
+           printf("No Avaliable Resources \n");
+           fflush(stdout);
+           return 0;       
+       } 
+       printf("Enter Lower Bound :- ");
+       fflush(stdout);
+       scanf("%d" , &x);
+       printf("Enter Upper Bound :- ");
+       fflush(stdout);
+       scanf("%d" , &y);
+       r = (y - x) / (numOfProcesses - 1);
+    } 
+    MPI_Bcast(&x, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&r, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    int result , cnt;
+    if(rank != 0){
+       cnt = 0 ;
+       int a = x + (rank - 1) * r;
+       int b = x + rank * r - 1;
+       for(int i = a ; i <= b ; ++i){
+          if(isPrime(i))cnt++;       
+       }
+    } 
+    MPI_Reduce(&cnt, &result, 1, MPI_INT, MPI_SUM, 0,MPI_COMM_WORLD);
+    if(rank == 0){  
+      for(int i = x + (numOfProcesses - 1) * r ; i <= y ; ++i){
+          if(isPrime(i)){
+             result++;
+          }      
+      }
+      printf("Total No Of Primes = %d.\n" , result);  
+      fflush(stdout);
+    }
+    MPI_Finalize();
+    return 0;
+}
+
